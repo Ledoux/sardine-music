c6adbdbef6b4a5f098f2a50f50bc79529b10739c
erwanledoux <lawanledoux@gmail.com>
2018-01-20 01:41:01 +0100

    added search box

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,15 +4,16 @@ import { connectSearchBox } from 'react-instantsearch/connectors';
 import Icon from './Icon'
 
 const SearchBox = ({ currentRefinement, refine }) => (
-  <div className="search-box col-6">
-    <input type="text"
+  <div className="search-box col-6 flex items-center">
+    <button className="button button--inversed button--alive">
+      <Icon icon='search' />
+    </button>
+    <input className="input"
+      type="text"
       value={currentRefinement}
       onChange={e => refine(e.target.value)}
       autoComplete="off"
     />
-    <button className="button button--alive">
-      <Icon icon='search' />
-    </button>
   </div>
 );
 
